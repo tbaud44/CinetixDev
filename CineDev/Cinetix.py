29053443e574ef90bfba177f05ba35bded75074e
Thierry baudouin <tb44_1@yahoo.fr>
2017-08-06 23:55:43 +0200

    init du projet

--- CineDev/Cinetix.py
+++ CineDev/Cinetix.py
@@ -3,20 +3,12 @@
 Created on 10 mai 2017
 
 @author: Thiery BAUDOUIN
-Programme principal lancant l'interface graphique initialise des données necessaires
+Programme principal lancant l'interface graphique qui initialise des données necessaires
 '''
 from Vue.BibliothequeVue import *
-import os
-import sys
 
 if __name__ == '__main__': 
     
-
-    if  len(sys.argv)==2 and sys.argv[1] == '-nodir':
-        #dans environnement eclipse, faire un chdir
-        #sous bat ou shell, se postionner dans le repertoire workspace/CINETIXV3 
-        os.chdir("C:/Users/HOME/git/CINETIXV3")
-   
     ihm = Interface()
     
     ihm.dessiner()
