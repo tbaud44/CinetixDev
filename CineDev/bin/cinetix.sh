800cb956071bde2ea0b956ca3bc3a45183cde517
Thierry BAUDOUIN <tb44_1@yahoo.fr>
2017-08-23 19:45:42 +0200

    maj script bin shell

--- CineDev/bin/cinetix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CINEPATH=C:/Users/HOME/workspace/CINETIXV3
-cd %CINEPATH%
-c:\Python36-32\python.exe Cinetix.py
---
+++ CineDev/bin/cinetixV3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+CINEPATH=/home/proj/git/CinetixDev/CineDev
+cd $CINEPATH
+python3.5 Cinetix.py
